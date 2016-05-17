0b72671334ebecd421a548c54c6c4b9e578ec880
Guannan Chen <kiritoalex@live.com>
2016-05-17 15:07:37 +0800

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -2,13 +2,13 @@
 
 sudo apt-get update
 sudo apt-get install curl libpcre3 libpcre3-dev zlib1g-dev libssl-dev build-essential git libxslt-dev libgd2-xpm-dev libgeoip-dev libpam-dev libperl-dev libxml2 libxml2-dev libxml2-utils libaprutil1 libaprutil1-dev apache2-prefork-dev libtool automake -y
-wget http://nginx.org/download/nginx-1.9.14.tar.gz && tar zxvf nginx-1.9.14.tar.gz
+wget http://nginx.org/download/nginx-1.9.15.tar.gz && tar zxvf nginx-1.9.15.tar.gz
 #PageSpeed
-wget https://github.com/pagespeed/ngx_pagespeed/archive/release-1.11.33.0-beta.zip -O pagespeed.zip
+wget https://github.com/pagespeed/ngx_pagespeed/archive/release-1.11.33.2-beta.zip -O pagespeed.zip
 unzip pagespeed.zip
 cd ngx_pagespeed-*
-wget https://dl.google.com/dl/page-speed/psol/1.11.33.0.tar.gz
-tar -xzvf 1.11.33.0.tar.gz
+wget https://dl.google.com/dl/page-speed/psol/1.11.33.2.tar.gz
+tar -xzvf 1.11.33.2.tar.gz
 cd ..
 #ModSecurity
 #wget https://www.modsecurity.org/tarball/2.9.1-rc1/modsecurity-2.9.1-RC1.tar.gz
@@ -25,8 +25,8 @@ unzip openssl.zip
 mv openssl-OpenSSL_1_0_2g/ openssl
 cd openssl && patch -p1 < ../sslconfig/patches/openssl__chacha20_poly1305_draft_and_rfc_ossl102g.patch
 cd ../
-wget -O nginx-ct.zip -c https://github.com/grahamedgecombe/nginx-ct/archive/v1.0.0.zip
+wget -O nginx-ct.zip -c https://github.com/grahamedgecombe/nginx-ct/archive/v1.2.0.zip
 unzip nginx-ct.zip
-cd nginx-1.9.14
-./configure --add-module=../nginx-ct-1.0.0 --with-openssl=../openssl --user=www-data --group=www-data --with-http_ssl_module --with-http_v2_module --with-ipv6 --add-module=../ngx_pagespeed-release-1.11.33.0-beta --with-stream 
+cd nginx-1.9.15
+./configure --add-module=../nginx-ct-1.2.0 --with-openssl=../openssl --user=www-data --group=www-data --with-http_ssl_module --with-http_v2_module --with-ipv6 --add-module=../ngx_pagespeed-release-1.11.33.2-beta --with-stream 
 make
